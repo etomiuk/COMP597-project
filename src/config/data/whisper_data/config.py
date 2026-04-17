cf7caae05eac29081ddc76c89b88284a0e71a141
etomiuk <emma.tomiuk@mail.mcgill.ca>
2026-04-17 13:14:50 -0400

    extra configs

--- src/config/data/whisper_data/config.py
+++ src/config/data/whisper_data/config.py
@@ -8,3 +8,4 @@ class DataConfig(_BaseConfig):
         self._arg_num_samples = _Arg(type=int, help="Number of samples for the synthetic dataset.", default=5500)
         self._arg_repeat = _Arg(type=int, help="Number of times to repeat the unique samples in the synthetic dataset.", default=1)
         self._arg_num_workers = _Arg(type=int, help="Number of workers for DataLoader", default=0)
+        self._arg_onfly = _Arg(type=str, help="Generate data on the fly vs before the training", default='n')
