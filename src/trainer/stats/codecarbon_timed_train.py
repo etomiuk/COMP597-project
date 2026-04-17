1569855dc8e3a4363a9ef090dc390cd9bdf1c087
etomiuk <emma.tomiuk@mail.mcgill.ca>
2026-04-17 14:55:49 -0400

    remove unnecessary classes from codecarbon timing train

--- src/trainer/stats/codecarbon_timed_train.py
+++ src/trainer/stats/codecarbon_timed_train.py
@@ -1,20 +1,13 @@
 from codecarbon import OfflineEmissionsTracker
-from codecarbon.core.util import backup
 from codecarbon.external.logger import logger
-from codecarbon.output_methods.base_output import BaseOutput
-from codecarbon.output_methods.emissions_data import EmissionsData, TaskEmissionsData
-from typing import List
 import codecarbon
 import codecarbon.core.cpu 
 import logging
 import os
-import csv
-import pandas as pd
 import src.config as config
 import src.trainer.stats.base as base
 import torch
 import src.trainer.stats.stats_data as stats
-
 
 logger = logging.getLogger(__name__)
 
@@ -30,104 +23,6 @@ def construct_trainer_stats(conf : config.Config, **kwargs) -> base.TrainerStats
         logger.warning("No device provided to codecarbon trainer stats. Using default PyTorch device")
         device = torch.get_default_device() 
     return CodeCarbonStats(device, conf.trainer_stats_configs.codecarbon.run_num, conf.trainer_stats_configs.codecarbon.project_name, conf.trainer_stats_configs.codecarbon.output_dir, conf)
-
-class SimpleFileOutput(BaseOutput): 
-    
-    def __init__(self, 
-        output_file_name: str = "codecarbon.csv", 
-        output_dir: str = ".",
-        on_csv_write: str = "append"
-    ):
-        if on_csv_write not in {"append", "update"}:
-            raise ValueError(
-                f"Unknown `on_csv_write` value: {on_csv_write}"
-                + " (should be one of 'append' or 'update'"
-            )
-        
-        self.output_file_name: str = output_file_name
-        
-        if not os.path.exists(output_dir):
-            raise OSError(f"Folder '{output_dir}' doesn't exist !")
-        
-        self.output_dir: str = output_dir
-        self.on_csv_write: str = on_csv_write
-        self.save_file_path = os.path.join(self.output_dir, self.output_file_name) #default: ./codecarbon.csv
-        
-        logger.info(
-        f"Emissions data (if any) will be saved to file {os.path.abspath(self.save_file_path)}"
-        )
-
-    def has_valid_headers(self, data: EmissionsData):
-        with open(self.save_file_path) as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            dict_from_csv = dict(list(csv_reader)[0])
-            list_of_column_names = list(dict_from_csv.keys())
-            return list(data.values.keys()) == list_of_column_names
-
-    def to_csv(self, total: EmissionsData, delta: EmissionsData):
-        """
-        Save the emissions data to a CSV file.
-        If the file already exists, append the new data to it.
-        param `delta` is not used in this method.
-        """
-
-        # Add code to check whether a part of the save_file_path already exists -->
-        # in our case, the output_file_name-experiment_name file exists, but the current code
-        # is only checking whether output_file_name exists. Since it doesn't, it goes ahead 
-        # and creates this new file, despite it being a "useless" file.
-
-        # Problem: stop() calls persist_data() which calls out() and task_out(). Thus the task csv file is accurate.
-        # But is out() accurate with tasks? How do tasks update total_emissions and delta? 
-
-        file_exists: bool = os.path.isfile(self.save_file_path)
-        
-        if file_exists and not self.has_valid_headers(total): # CSV headers changed
-            logger.warning("The CSV format have changed, backing up old emission file.")
-            backup(self.save_file_path)
-            file_exists = False 
-        
-        new_df = pd.DataFrame.from_records([dict(total.values)])
-
-        if not file_exists:
-            df = new_df
-        elif self.on_csv_write == "append":
-            df = pd.read_csv(self.save_file_path)
-            df = pd.concat([df, new_df])
-        else:
-            df = pd.read_csv(self.save_file_path)
-            df_run = df.loc[df.run_id == total.run_id]
-            if len(df_run) < 1:
-                df = pd.concat([df, new_df])
-            elif len(df_run) > 1:
-                logger.warning(
-                f"CSV contains more than 1 ({len(df_run)})"
-                + f" rows with current run ID ({total.run_id})."
-                + "Appending instead of updating."
-                )
-                df = pd.concat([df, new_df])
-            else:
-                df.at[df.run_id == total.run_id, total.values.keys()] = total.values.values()
-    
-        df.to_csv(self.save_file_path, index=False)
-
-    def out(self, total: EmissionsData, delta: EmissionsData):
-        self.to_csv(total, delta)
-
-    def live_out(self, total: EmissionsData, delta: EmissionsData):
-        pass
-
-    def task_out(self, data: List[TaskEmissionsData], experiment_name: str):
-        # run_id = data[0].run_id
-        split = os.path.splitext(self.save_file_path)
-        save_task_file_path = split[0] + "-" + experiment_name + split[1]
-        df = pd.DataFrame(columns=data[0].values.keys())
-        new_df = pd.DataFrame.from_records(
-            [dict(data_point.values) for data_point in data]
-        )
-        # Filter out empty or all-NA columns, to avoid warnings from Pandas
-        new_df = new_df.dropna(axis=1, how="all")
-        df = pd.concat([df, new_df], ignore_index=True)
-        df.to_csv(save_task_file_path, index=False)
 
 
 class CodeCarbonStats(base.TrainerStats):
